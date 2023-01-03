aca3e8ee4f6d7e2d5f9db6a819dcb1294402c70a
ThienLe2112 <120434264+ThienLe2112@users.noreply.github.com>
2023-01-03 22:04:36 +0700

    Add files via upload

---
+++ MotorFibo.ino
@@ -0,0 +1,209 @@
+#include <Arduino_FreeRTOS.h>
+#include<string.h>
+// vitrimm=120 di dc 30cm
+// vitrimm =75 xoay goc 90 do
+int vitril = 0, vitrir = 0, vitrimml = 0, vitrimmr = 0, loil, loir;
+#define in1 5
+#define in2 6
+#define in3 9
+#define in4 10
+int ss1;
+int ss2;
+int ss3;
+int ss4;
+int ss0;
+#define dl1 8
+#define dl2 13
+#define dl3 4
+#define dl4 7
+void chayr() {
+  if ( digitalRead(12) == 1) {
+    vitrir--;
+  }
+  else
+    vitrir++;
+
+}
+void chayl() {
+  if ( digitalRead(11) == 1) {
+    vitril++;
+  }
+  else
+    vitril--;
+
+}
+void quayl(int nangluongl) {
+  if (nangluongl >= 150) {
+    nangluongl = 150;
+  }
+  if (nangluongl <= -150) {
+    nangluongl = -150;
+  }
+  if (nangluongl > 0) {
+    analogWrite(in3, 0);
+    analogWrite(in4, nangluongl);
+  }
+  if (nangluongl < 0) {
+    analogWrite(in3, -nangluongl);
+    analogWrite(in4, 0);
+  }
+}
+void quayr(int nangluongr) {
+  if (nangluongr >= 150) {
+    nangluongr = 150;
+  }
+  if (nangluongr <= -150) {
+    nangluongr = -150;
+  }
+  if (nangluongr > 0) {
+    analogWrite(in1, 0);
+    analogWrite(in2, nangluongr);
+  }
+  if (nangluongr < 0) {
+    analogWrite(in1, -nangluongr);
+    analogWrite(in2, 0);
+  }
+}
+void quayc(int nangluongc) {
+  if (nangluongc >= 150) {
+    nangluongc = 150;
+  }
+  if (nangluongc <= -150) {
+    nangluongc = -150;
+  }
+  if (nangluongc > 0) {
+    analogWrite(in1, 0);
+    analogWrite(in2, nangluongc);
+    analogWrite(in3, 0);
+    analogWrite(in4, nangluongc);
+
+  }
+  if (nangluongc < 0) {
+    analogWrite(in1, -nangluongc);
+    analogWrite(in2, 0);
+    analogWrite(in3, -nangluongc);
+    analogWrite(in4, 0);
+
+  }
+}
+void xoay(int goc) {
+  switch (goc) {
+    case 90: {
+        vitrimml += 75;
+        vitrimmr -= 75;
+        quayl(pid(loil, 3, 0, 1));
+        quayr(pid(loir, 3, 0, 1));
+        break;
+      }
+
+    case -90: {
+        vitrimml -= 75;
+        vitrimmr += 75;
+        quayl(pid(loil, 3, 0, 1));
+        quayr(pid(loir, 3, 0, 1));
+        break;
+      }
+
+
+
+  }
+}
+void stopmotor() {
+  analogWrite(in1, 0);
+  analogWrite(in2, 0);
+  analogWrite(in3, 0);
+  analogWrite(in4, 0);
+}
+int pid(int loi, int kp, int ki, int kd) {
+  int dloi ;
+  static int  loitr = 0, iloi = 0;
+  long int temp;
+  dloi = loi - loitr;
+  iloi += loi;
+  if (iloi >= 100) {
+    iloi = 100;
+  }
+  if (iloi <= -100) {
+    iloi = -100;
+  }
+  loitr = loi;
+  temp = kp * loi + ki * iloi + kd * dloi;
+  if (temp >= 255) {
+    temp = 255;
+  }
+  if (temp <= -255) {
+    temp = -255;
+  }
+  return temp;
+}
+void dithang(int nangluong) {
+  vitrimml = 120;
+  loil = vitrimml - vitril;
+  vitrimmr = 120;
+  loir = vitrimmr - vitrir;
+  quayl(pid(loil, 3, 0, 1));
+  quayr(pid(loir, 3, 0, 1));
+}
+
+void setup() {
+  attachInterrupt(1, chayr, RISING);
+  attachInterrupt(0, chayl, RISING);
+  Serial.begin(9600);
+}
+
+void loop() {
+  Serial.print("Vi tri left " ); Serial.println(vitril);
+  Serial.print("Vi tri right " ); Serial.println(vitrir);
+  ss0 = analogRead(A0);
+  ss1 = analogRead(A1);
+  ss2 = analogRead(A2);
+  ss3 = analogRead(A3);
+  ss4 = analogRead(A4);
+  vitrimml = 120; vitrimmr = 120;
+  loil = vitrimml - vitril;
+  loir = vitrimmr - vitrir;
+  while (loil < -5 || loil > 5) {
+    attachInterrupt(1, chayr, RISING);
+    attachInterrupt(0, chayl, RISING);
+    Serial.print("Vi tri left " ); Serial.println(vitril);
+    Serial.print("Vi tri right " ); Serial.println(vitrir);
+    vitrimml = 120; vitrimmr = 120;
+    loil = vitrimml - vitril;
+    loir = vitrimmr - vitrir;
+    quayl(pid(loil, 3, 0, 1));
+    quayr(pid(loir, 3, 0, 1));
+  }
+  if (ss3 > 100) {
+    int vitrixoayl = 75 + vitril;
+    int vitrixoayr = -75 + vitrir;
+    while (loil > -10 || loil < 10) {
+      attachInterrupt(1, chayr, RISING);
+      attachInterrupt(0, chayl, RISING);
+      Serial.print("Vi tri left " ); Serial.println(vitril);
+      Serial.print("Vi tri right " ); Serial.println(vitrir);
+      loil = vitrixoayl - vitril;
+      loir = vitrixoayr - vitrir;
+      quayl(pid(loil, 3, 0, 1));
+      quayr(pid(loir, 3, 0, 1));
+    }
+  }
+  //  Serial.println("SS0 " + String(ss1));
+  //  Serial.println("SS1 " + String(ss2));
+  //  Serial.println("SS2 " + String(ss3));
+  //  Serial.println("SS3 " + String(ss4));
+  //  Serial.println("SS4 " + String(ss5));
+  //  if (ss4 > 50) {
+  //    stopmotor();
+  //    do {
+  //
+  //      ss1 = analogRead(A0);
+  //      Serial.println("Quay phai");
+  //    } while (ss4 > 50);
+  //    Serial.println("Dung lai");
+  //    stopmotor();
+  //  }
+  //  else if (ss2 < 50) {
+  //    do
+  //    }
+
+}
---
+++ sumo.ino
@@ -0,0 +1,140 @@
+// Le Quang Anh   11/22/2022
+
+#define motorTrai_in1   5
+#define motorTrai_in2   6
+#define motorPhai_in3   9
+#define motorPhai_in4   10
+#define doLine_Truoc_Trai   4
+#define doLine_Truoc_Phai   7
+#define doLine_Sau_Phai     11
+#define doLine_Sau_Trai     8
+
+// m chinh lai chan tu dong  11 den dong 13 nha =))))
+#define SS_CanBang_Trai     A0
+#define SS_KhoangCach_Trai  A1
+#define SS_KhoangCach_Giua  A2
+#define SS_KhoangCach_Phai  A3
+#define SS_CanBang_Phai     A4
+// macro for sumo battle
+#define MAX_SPEED           255
+#define MIN_SPEED           50
+#define NORMAL_SPEED        150
+#define PhatHien_DoiThu     1
+#define KILL                (chayThang(MAX_SPEED,0))
+#define VACH        1
+//macro for maze
+#define WALL
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(motorTrai_in1, OUTPUT);
+  pinMode(motorTrai_in2, OUTPUT);
+  pinMode(motorTrai_in1, OUTPUT);
+  pinMode(motorTrai_in2, OUTPUT);
+  pinMode(doLine_Truoc_Trai, INPUT);
+  pinMode(doLine_Truoc_Phai, INPUT);
+  pinMode(doLine_Sau_Trai, INPUT);
+  pinMode(doLine_Sau_Phai, INPUT);
+  pinMode(SS_KhoangCach_Giua, INPUT);
+  pinMode(SS_KhoangCach_Trai, INPUT);
+  pinMode(SS_KhoangCach_Phai, INPUT);
+  pinMode(SS_CanBang_Trai, INPUT);
+  pinMode(SS_CanBang_Phai, INPUT);
+  Serial.begin(9600);
+  Serial.println("Oke");
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  chayThang(NORMAL_SPEED, 0);
+
+  if (digitalRead(SS_KhoangCach_Giua) == PhatHien_DoiThu )
+  {
+    do {
+      digitalRead(SS_KhoangCach_Giua);
+      Serial.println("KILL");
+      KILL;
+
+    }
+    while (digitalRead(SS_KhoangCach_Giua) == PhatHien_DoiThu);
+  }
+  else if (digitalRead(SS_KhoangCach_Trai) == PhatHien_DoiThu)
+  {
+    reTrai(MAX_SPEED, 100);
+    Serial.println("reTrai(MAX_SPEED, 100)");
+    do
+    {
+      digitalRead(SS_KhoangCach_Giua);
+
+      if (digitalRead(SS_KhoangCach_Giua) == PhatHien_DoiThu )
+      {
+        Serial.println("KILL");
+        break;
+      }
+    }
+    while (digitalRead(SS_KhoangCach_Giua) == PhatHien_DoiThu);
+    KILL;
+  }
+  else if (digitalRead(SS_KhoangCach_Phai) == PhatHien_DoiThu)
+  {
+    rePhai(MAX_SPEED, 100);
+    Serial.println("rePhai(MAX_SPEED, 100)");
+    do
+    {
+      digitalRead(SS_KhoangCach_Giua);
+
+      if (digitalRead(SS_KhoangCach_Giua) == PhatHien_DoiThu )
+      {
+        Serial.println("KILL");
+        break;
+      }
+    }
+    while (digitalRead(SS_KhoangCach_Giua) == PhatHien_DoiThu);
+    Serial.println("KILL");
+
+    KILL;
+  }
+  else if (digitalRead(doLine_Sau_Phai) == VACH || digitalRead(doLine_Sau_Trai) == VACH )
+  {
+    chayThang(MAX_SPEED, 0);
+  }
+  else if (digitalRead(doLine_Truoc_Phai) == VACH || digitalRead(doLine_Truoc_Trai) == VACH)
+  {
+    goBack(MAX_SPEED, 100);
+    rePhai(MAX_SPEED, 300);
+  }
+}
+
+void chayThang(int speed, int timeout)
+{
+  analogWrite(motorTrai_in1, speed);
+  analogWrite(motorTrai_in2, 0);
+  analogWrite(motorPhai_in3, 0);
+  analogWrite(motorPhai_in4, speed);
+  delay(timeout);
+}
+void rePhai(int speed, int timeout)
+{
+  analogWrite(motorTrai_in1, speed);
+  analogWrite(motorTrai_in2, 0);
+  analogWrite(motorPhai_in3, speed);
+  analogWrite(motorPhai_in4, 0);
+  delay(timeout);
+}
+void reTrai(int speed, int timeout)
+{
+  analogWrite(motorTrai_in1, 0);
+  analogWrite(motorTrai_in2, speed);
+  analogWrite(motorPhai_in3, 0);
+  analogWrite(motorPhai_in4, speed);
+  delay(timeout);
+}
+void goBack(int speed, int timeout)
+{
+  analogWrite(motorTrai_in1, 0);
+  analogWrite(motorTrai_in2, speed);
+  analogWrite(motorPhai_in3, speed);
+  analogWrite(motorPhai_in4, 0);
+  delay(timeout);
+}
+
